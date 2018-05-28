497508848af623d2fdfd201b63198092646f4f69
jowj <me@jowj.net>
2018-05-28 18:14:54 -0500

    Update operating order of script.

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+# change default umask so that I can actually use my ssh keys
+umask 077
+
 # prompt for decrypt and extract secrets
 gpg --output ~/.ssh.tar --decrypt /colove/setup/secrets/ssh-secrets.tar.gpg
 mkdir .ssh
 7z x ~/.ssh.tar
 
-# change default umask so that I can actually use my ssh keys
-umask 077
 # start up the ssh agent.
 eval `ssh-agent`
