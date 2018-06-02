36942d312b980be0b1fee768fac003338a06e8cd
jowj <me@jowj.net>
2018-06-02 10:54:14 -0500

    Set unix style line endings in repo.

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # change default umask so that I can actually use my ssh keys
 umask 077
-
 # prompt for decrypt and extract secrets
 gpg --output ~/.ssh.tar --decrypt /colove/setup/secrets/ssh-secrets.tar.gpg
 7z x ~/.ssh.tar
